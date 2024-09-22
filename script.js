bc900e290fd8215b6bbf1a41ab3086dd0551ca70
joaoviscaino <joao-viscaino@hotmail.com>
2024-09-21 21:37:12 -0400

    Finished the game. Added a playGame() function that contained a function declaration, which is definitely a different way to program than what I am accustomed to

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function getComputerChoice() {
     /* Here we're storing the computer number by invoking the random function */
     let number = random(); 
     switch (number) {
-        case 1:
+    case 1:
             return "Rock";
-        case 2:
+    case 2:
             return "Paper";
-        case 3:
+    case 3:
             return "Scissors";
         default:
-            return "None(?)";
-    }
+            return "";
+    }  
 }
 
 function getHumanChoice() {
@@ -26,35 +26,93 @@ function getHumanChoice() {
             2 for Paper
             3 for Scissors`);
         })() ) {
-        case '1':
+    case '1':
             return "Rock";
-        case '2':
+    case '2':
             return "Paper";
-        case '3':
+    case '3':
             return "Scissors";
         default:
-            break;
+            return "";
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toUpperCase();
-}
+/* Using a JS Object for the choises and utilizing key:value pairs makes the code a LOT
+more readable and shorter, but here we are: */
 
-let humanScore = 0, computerScore = 0;
-const humanCh = getHumanChoice();
-const computerCh = getComputerChoice();
 
-for (let index = 0; index < 5; index++) {
+function playGame () {
+    /* local function */
+    function playRound() {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
     
+        if (humanChoice === "Rock" && computerChoice === "Paper") {
+            /* Human choosing Rock options */
+            computerScore += 1;
+            console.log(`Player chose: ${humanChoice},
+                \nComputer chose: ${computerChoice}.
+                \nComputer scores 1.`);
+        }
+        else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+            humanScore += 1;
+            console.log(`Player chose: ${humanChoice},
+                \nComputer chose: ${computerChoice}.
+                \nPlayer scores 1.`);
+        }
+            /* Human choosing Paper options */
+        else if (humanChoice === "Paper" && computerChoice === "Rock") {
+            humanScore += 1;
+            console.log(`Player chose: ${humanChoice},
+                \nComputer chose: ${computerChoice}.
+                \nPlayer scores 1.`);
+        }
+        else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+            computerScore += 1;
+            console.log(`Player chose: ${humanChoice},
+                \nComputer chose: ${computerChoice}.
+                \nComputer scores 1.`);
+        }
+            /* Human choosing Scissors options */
+        else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+            humanScore += 1;
+            console.log(`Player chose: ${humanChoice},
+                \nComputer chose: ${computerChoice}.
+                \nPlayer scores 1.`);
+        }
+        else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+            computerScore += 1;
+            console.log(`Player chose: ${humanChoice},
+                \nComputer chose: ${computerChoice}.
+                \nComputer scores 1.`);
+        }
+        else {
+            console.log("Draw!");
+        }
+        
+    }
+    let humanScore = 0, computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+       playRound();
+    }
+    console.log(`Player points: ${humanScore}. Computer points: ${computerScore}`);
+    
+    /* display who won: */
+
+    if (humanScore === computerScore) {console.log("No one wins. How unexpected!");}
+    else if (humanScore > computerScore) {console.log("Player wins!");}
+    else if (humanScore < computerScore) {console.log("Computer wins!");}
 }
 
-console.log(getComputerChoice());
+
+
+/* Every simple game is built with a similar structure */
+playGame();
 
 /*
 console.log((function() {
     switch (key) {
-        case value:
+    else if value:
             
             break;
     
